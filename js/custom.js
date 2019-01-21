0a458e0396ee070858c07940345ee1f08dc0f99c
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2019-01-21 01:26:34 +0100

    insert carousel to Events

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,32 @@ $(window).scroll(function() {
  
   }
 });
+$(document).ready(function(){
+  $('.carouselSlick').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    speed: 300,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
