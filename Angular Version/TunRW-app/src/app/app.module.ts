a12e2095419f19d1e29974f42516203386825b07
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2019-02-24 14:05:22 +0100

    Add First Component

--- Angular Version/TunRW-app/src/app/app.module.ts
+++ Angular Version/TunRW-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectCreateComponent } from './projects/project-create/project-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular Version/TunRW-app/src/app/projects/project-create/project-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-create',
+  templateUrl: './project-create.component.html',
+  styleUrls: ['./project-create.component.css']
+})
+
+
+export class ProjectCreateComponent {
+
+}
