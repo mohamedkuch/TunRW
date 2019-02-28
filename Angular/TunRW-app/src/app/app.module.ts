6f08116592ac3d86bb709d2fa77d235cd05e6a01
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2019-02-28 18:44:45 +0100

    update new Angular version ( header + services)

--- Angular/TunRW-app/src/app/app.module.ts
+++ Angular/TunRW-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopCoverComponent } from './topcover/topcover.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent ,
+    TopCoverComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
