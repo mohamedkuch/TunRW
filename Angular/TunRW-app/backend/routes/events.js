85150c759be7969b0ffa48fbe0683996135fa273
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2019-05-18 02:08:59 +0200

    Add Filter to edit events

--- Angular/TunRW-app/backend/routes/events.js
+++ Angular/TunRW-app/backend/routes/events.js
@@ -107,9 +107,13 @@ router.put('/:id',checkAuth, multer({storage : storage}).single("image"), (req, 
     imagePath : imageURL
   });
   console.log(post);
-  Event.updateOne({_id: req.params.id}, post).then(result =>{
-    res.status(200).json({  message : "Post updated!"});
-  })
+  Event.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result =>{
+    if(result.nModified > 0){
+      res.status(200).json({ message: "Update Successful !"});
+    }else {
+      res.status(401).json({  message : "Not Authorized!"});
+    }
+   });
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
