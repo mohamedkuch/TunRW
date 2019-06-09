3fd6296c7c17d6cb981592d4353c176e9716e3cd
mohamed kechaou <medksbuss@gmail.com>
2019-06-09 21:27:05 +0200

    optimize backend

--- Angular/TunRW-app/backend/routes/events.js
+++ Angular/TunRW-app/backend/routes/events.js
@@ -1,141 +1,17 @@
 const express = require("express");
-const Event = require("../models/events");
-const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
-
+const extractFile = require('../middleware/file');
+const eventController = require("../controller/events");
 const router = express.Router();
-const MIME_TYPE_MAP = {
-  'image/png' : 'png',
-  'image/jpeg' : 'jpg',
-  'image/jpg' : 'jpg'
-}
-const storage = multer.diskStorage({
-  destination: (req , file , cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime Type");
-    if (isValid){
-      error = null;
-    }
-    cb(null, "backend/images");
-  },
-  filename: (req , file , cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
 
-router.post("", checkAuth, multer({storage : storage}).single("image"),(req,res,next) => {
-  const url = req.protocol + '://' + req.get("host");
-  const post = new Event({
-    title : req.body.title,
-    date : req.body.date,
-    adress : req.body.adress,
-    description : req.body.description,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added Successfully',
-      event: {
-       /*
-        title: result.title,
-        date: result.date,     // Equal To
-        adress: result.adress,
-        description: result.description,
-        imagePath: result.imagePath*/
-        ...result,
-        id: result._id
-      }
-    });
-  })
-  .catch(err =>{
-    res.status(500).json({
-      message : "Creating Event Failed!"
-    });
-  });
+router.post("", checkAuth, extractFile,eventController.createEvent);
 
-});
+router.get('', eventController.getAllEvents);
 
-router.get('', (req, res, next) => {
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.page;
-  const postQuery = Event.find();
-  let fetchedPosts;
-  if( pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
-  }
-  postQuery.find().then(documents => {
-        fetchedPosts = documents
-        return Event.count();
-    }).then(count =>{
-      res.status(200).json({
-        message: 'Events fetched Succesfully!',
-        events: fetchedPosts,
-        maxPosts: count
-      });
-    });
+router.get('/:id', extractFile, eventController.getOneEvent);
 
-});
+router.put('/:id',checkAuth, extractFile, eventController.updateEvent);
 
-router.get('/:id', multer({storage : storage}).single("image"), (req, res, next) => {
-  Event.findById(req.params.id).then(post =>{
-   if(post){
-      res.status(200).json(post);
-   } else {
-     res.status(404).json({message: 'Event not found!'});
-   }
-
-  });
-
-
-});
-
-router.put('/:id',checkAuth, multer({storage : storage}).single("image"), (req, res, next) => {
-  let imageURL = req.body.imagePath;
-  if(req.file){
-    const url = req.protocol + '://' + req.get("host");
-    imageURL = url + "/images/" + req.file.filename;
-  }
-  const post = new Event({
-    _id: req.body.id,
-    title : req.body.title,
-    date : req.body.date,
-    adress : req.body.adress,
-    description : req.body.description,
-    imagePath : imageURL,
-    creator: req.userData.userId
-  });
-  console.log(post);
-  Event.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result =>{
-    if(result.nModified > 0){
-      res.status(200).json({ message: "Update Successful !"});
-    }else {
-      res.status(401).json({  message : "Not Authorized!"});
-    }
-   }).catch(error => {
-    res.status(500).json({
-      message : "Update Event Failed!"
-    });
-  });
-});
-
-router.delete('/:id', checkAuth, (req, res, next) => {
-  Event.deleteOne({_id:req.params.id, creator: req.userData.userId}).then(result =>{
-    if(result.n > 0){
-      res.status(200).json({ message: "Event Deleted !"});
-    }else {
-      res.status(401).json({  message : "Not Authorized!"});
-    }
-  }).catch(error => {
-    res.status(500).json({
-      message : "Deleting Event Failed!"
-    });
-  });
-
-});
+router.delete('/:id', checkAuth, eventController.deleteEvent);
 
 module.exports = router;
--- Angular/TunRW-app/backend/routes/user.js
+++ Angular/TunRW-app/backend/routes/user.js
@@ -5,102 +5,12 @@ const jwt = require('jsonwebtoken');
 const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
+const userController = require("../controller/user");
 
-router.post("/signup", checkAuth, (req, res, next) =>{
-  bcrypt.hash(req.body.password, 10)
-    .then(hash =>{
-      const user = new User({
-        username: req.body.username,
-        name: req.body.name,
-        password: hash
-      });
-      user.save()
-        .then(result =>{
-          res.status(201).json({
-            message: 'User Created!',
-            result: result
-          });
-        })
-        .catch(err =>{
-          res.status(500).json({
-            message: 'Create User failed, Username is taken !'
-          });
-        });
-    });
+router.post("/signup", checkAuth,userController.createUser );
+router.post("/login", userController.loginUser);
 
+router.get('', userController.getAllMembers);
+router.get('/:id', userController.getOneMember);
 
-});
-router.post("/login", (req, res, next) =>{
-  let fetchedUser;
-  User.findOne({username: req.body.username })
-    .then(user =>{
-      if(!user) {
-        return res.status(401).json({
-          message: 'Login failed'
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    }).then ( result => {
-      if(!result){
-        return res.status(401).json({
-          message: 'Login failed , password is incorrect'
-        });
-      }
-      const token = jwt.sign({username: fetchedUser.username, userId: fetchedUser._id},
-         'TunRW_Funcheta_Studio_komos_nikos',
-         {expiresIn: '1h' }
-         );
-      return res.status(200).json({
-        token: token,
-        expiresIn: 3600,
-        id: fetchedUser._id,
-        username: fetchedUser.username,
-        name: fetchedUser.name
-      });
-
-    }).catch (err => {
-      return res.status(401).json({
-        message: 'Login failed , Username or password are incorrect'
-      });
-    })
-
-});
-
-router.get('', (req, res, next) => {
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.page;
-  const postQuery = User.find();
-  let fetchedPosts;
-  if( pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
-  }
-  postQuery.find().then(documents => {
-        fetchedPosts = documents
-        return User.count();
-    }).then(count =>{
-      res.status(200).json({
-        message: 'Members fetched Succesfully!',
-        users: fetchedPosts,
-        maxPosts: count
-      });
-    });
-
-});
-
-
-router.get('/:id', (req, res, next) => {
-  User.findById(req.params.id).then(userData =>{
-   if(userData){
-      res.status(200).json(userData);
-   } else {
-     res.status(404).json({message: 'Event not found!'});
-   }
-
-  });
-
-
-});
 module.exports = router;
