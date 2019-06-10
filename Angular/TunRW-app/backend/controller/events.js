606ee69df42a4defed8293d3b1621b0ca4edbdca
mohamed kechaou <medksbuss@gmail.com>
2019-06-10 23:31:30 +0200

    finish

--- Angular/TunRW-app/backend/controller/events.js
+++ Angular/TunRW-app/backend/controller/events.js
@@ -79,7 +79,6 @@ exports.createEvent = (req,res,next) => {
       imagePath : imageURL,
       creator: req.userData.userId
     });
-    console.log(post);
     Event.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result =>{
       if(result.n > 0){
         res.status(200).json({ message: "Update Successful !"});
