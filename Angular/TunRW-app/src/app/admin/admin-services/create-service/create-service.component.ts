1aa5b915e49f3bc1dd9a9d334be6f1669d30a5c9
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2019-08-15 13:52:57 +0200

    finish slider for creating services

--- Angular/TunRW-app/src/app/admin/admin-services/create-service/create-service.component.ts
+++ Angular/TunRW-app/src/app/admin/admin-services/create-service/create-service.component.ts
@@ -24,7 +24,7 @@ export class CreateServiceComponent implements OnInit, AfterViewChecked {
   service: Service;
   isLoading = false;
   form: FormGroup;
-  fontAwesomeList = [   "fas fa-address-book","fas fa-address-card","fas fa-adjust","fas fa-align-center","fas fa-align-justify","fas fa-align-left","fas fa-align-right","fas fa-allergies","fas fa-ambulance","fas fa-american-sign-language-interpreting","fas fa-anchor","fas fa-angle-double-down","fas fa-angle-double-left","fas fa-angle-double-right","fas fa-angle-double-up","fas fa-angle-down","fas fa-angle-left","fas fa-angle-right","fas fa-angle-up","fas fa-archive","fas fa-arrow-alt-circle-down","fas fa-arrow-alt-circle-left","fas fa-arrow-alt-circle-right","fas fa-arrow-alt-circle-up","fas fa-arrow-circle-down","fas fa-arrow-circle-left","fas fa-arrow-circle-right","fas fa-arrow-circle-up","fas fa-arrow-down","fas fa-arrow-left","fas fa-arrow-right","fas fa-arrow-up","fas fa-arrows-alt","fas fa-arrows-alt-h","fas fa-arrows-alt-v","fas fa-assistive-listening-systems","fas fa-asterisk","fas fa-at","fas fa-audio-description","fas fa-backward","fas fa-balance-scale","fas fa-ban","fas fa-band-aid","fas fa-barcode","fas fa-bars","fas fa-baseball-ball","fas fa-basketball-ball","fas fa-bath","fas fa-battery-empty","fas fa-battery-full","fas fa-battery-half","fas fa-battery-quarter","fas fa-battery-three-quarters","fas fa-bed","fas fa-beer","fas fa-bell","fas fa-bell-slash","fas fa-bicycle","fas fa-binoculars","fas fa-birthday-cake","fas fa-blind","fas fa-bold","fas fa-bolt","fas fa-bomb","fas fa-book","fas fa-bookmark","fas fa-bowling-ball","fas fa-box","fas fa-box-open","fas fa-boxes","fas fa-braille","fas fa-briefcase","fas fa-briefcase-medical","fas fa-bug","fas fa-building","fas fa-bullhorn","fas fa-bullseye","fas fa-burn","fas fa-bus","fas fa-calculator","fas fa-calendar","fas fa-calendar-alt","fas fa-calendar-check","fas fa-calendar-minus","fas fa-calendar-plus"
+  fontAwesomeList = ["fas fa-address-book","fas fa-address-card","fas fa-adjust","fas fa-align-center","fas fa-align-justify","fas fa-align-left","fas fa-align-right","fas fa-allergies","fas fa-ambulance","fas fa-american-sign-language-interpreting","fas fa-anchor","fas fa-angle-double-down","fas fa-angle-double-left","fas fa-angle-double-right","fas fa-angle-double-up","fas fa-angle-down","fas fa-angle-left","fas fa-angle-right","fas fa-angle-up","fas fa-archive","fas fa-arrow-alt-circle-down","fas fa-arrow-alt-circle-left","fas fa-arrow-alt-circle-right","fas fa-arrow-alt-circle-up","fas fa-arrow-circle-down","fas fa-arrow-circle-left","fas fa-arrow-circle-right","fas fa-arrow-circle-up","fas fa-arrow-down","fas fa-arrow-left","fas fa-arrow-right","fas fa-arrow-up","fas fa-arrows-alt","fas fa-arrows-alt-h","fas fa-arrows-alt-v","fas fa-assistive-listening-systems","fas fa-asterisk","fas fa-at","fas fa-audio-description","fas fa-backward","fas fa-balance-scale","fas fa-ban","fas fa-band-aid","fas fa-barcode","fas fa-bars","fas fa-baseball-ball","fas fa-basketball-ball","fas fa-bath","fas fa-battery-empty","fas fa-battery-full","fas fa-battery-half","fas fa-battery-quarter","fas fa-battery-three-quarters","fas fa-bed","fas fa-beer","fas fa-bell","fas fa-bell-slash","fas fa-bicycle","fas fa-binoculars","fas fa-birthday-cake","fas fa-blind","fas fa-bold","fas fa-bolt","fas fa-bomb","fas fa-book","fas fa-bookmark","fas fa-bowling-ball","fas fa-box","fas fa-box-open","fas fa-boxes","fas fa-braille","fas fa-briefcase","fas fa-briefcase-medical","fas fa-bug","fas fa-building","fas fa-bullhorn","fas fa-bullseye","fas fa-burn","fas fa-bus","fas fa-calculator","fas fa-calendar","fas fa-calendar-alt","fas fa-calendar-check","fas fa-calendar-minus","fas fa-calendar-plus"
 ];
   activeIconList : any;
   activeCounterStart = 0;
@@ -83,8 +83,19 @@ export class CreateServiceComponent implements OnInit, AfterViewChecked {
         this.activeIconList.unshift("");
 
     }
+    // previous Last Element
+    if(this.activeSliderCounter == this.fontAwesomeList.length-2) {
+      this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 4);
+      this.activeIconList.push("noClass");
+    }
+    // Last Element
+    if(this.activeSliderCounter == this.fontAwesomeList.length-1) {
+      this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 3);
+      this.activeIconList.push("noClass");
+      this.activeIconList.push("noClass");
+    }
 
-    console.log("setting up", this.activeCounterStart, this.activeSliderCounter);
+    console.log("setting up", this.fontAwesomeList.length, this.activeSliderCounter);
     console.log("Array", this.activeIconList);
     this.setServicesIcons();
 
@@ -96,20 +107,59 @@ export class CreateServiceComponent implements OnInit, AfterViewChecked {
     }
   }
   onNextClick(){
-    this.activeCounterStart ++;
-    this.activeSliderCounter++;
-    this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart+ 5);
-    this.setServicesIcons();
+    if(this.activeSliderCounter < this.fontAwesomeList.length -1) {
+      this.activeCounterStart ++;
+      this.activeSliderCounter++;
+      if(this.activeSliderCounter == 1){
+        this.activeIconList = this.fontAwesomeList.slice(0,4);
+        this.activeIconList.unshift("");
+      }
+      if(this.activeSliderCounter == this.fontAwesomeList.length-1){
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 3);
+        this.activeIconList.push("noClass");
+        this.activeIconList.push("noClass");
+      }
+      if(this.activeSliderCounter == this.fontAwesomeList.length - 2) {
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 4);
+        this.activeIconList.push("noClass");
+      }
+      if(this.activeSliderCounter <= this.fontAwesomeList.length-3 && this.activeSliderCounter >= 2) {
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart+ 5);
+      }
+      this.setServicesIcons();
+    }
   }
   onPrevClick(){
-    this.activeCounterStart --;
-    this.activeSliderCounter--;
-    this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart+ 5);
-    this.setServicesIcons();
+    if(this.activeSliderCounter > 0 ){
+      this.activeCounterStart --;
+      this.activeSliderCounter--;
+      if(this.activeSliderCounter == 1){
+        this.activeIconList = this.fontAwesomeList.slice(0,4);
+        this.activeIconList.unshift("");
+      }else if(this.activeSliderCounter == 0){
+        this.activeIconList = this.fontAwesomeList.slice(0,3);
+        this.activeIconList.unshift("");
+        this.activeIconList.unshift("");
+      }else if(this.activeSliderCounter == this.fontAwesomeList.length - 2) {
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 4);
+        this.activeIconList.push("noClass");
+      }else if(this.activeSliderCounter == this.fontAwesomeList.length - 1) {
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart + 4);
+        this.activeIconList.push("noClass");
+        this.activeIconList.push("noClass");
+      }
+      else {
+        this.activeIconList = this.fontAwesomeList.slice(this.activeCounterStart,this.activeCounterStart+ 5);
+        this.setServicesIcons();
+      }
+     
+    }
+
 
   }
   setServicesIcons(){
-    console.log("setting Element", this.firstIcon);
+    console.log("setting up" ,this.activeIconList);
+
     this.firstIcon.nativeElement.firstChild.className = "serviceIcon";
     this.scdIcon.nativeElement.firstChild.className = "fa-2x serviceIcon ";
     this.thirdIcon.nativeElement.firstChild.className = "fa-3x serviceIcon ";
