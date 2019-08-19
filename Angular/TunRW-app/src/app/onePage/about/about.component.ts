fc24b237bac2fa3d5ddb8acca9dcf62bc7a73373
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2019-08-19 04:06:12 +0200

    fix up creation design

--- Angular/TunRW-app/src/app/onePage/about/about.component.ts
+++ Angular/TunRW-app/src/app/onePage/about/about.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit, OnDestroy } from '@angular/core';
 import { SlickCarouselComponent } from 'ngx-slick-carousel';
+import { TeamMembers } from 'src/app/admin/admin-about/teamMeambers.modal';
+import { Subscription } from 'rxjs';
+import { TeamMembersService } from 'src/app/admin/admin-about/teamMembers.service';
 
 @Component({
   selector : 'app-about',
@@ -7,13 +10,17 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
   styleUrls : ['./about.component.scss']
 })
 
-export class AboutComponent implements AfterViewInit{
-  ngAfterViewInit(): void {
-    this.slickModal.slickGoTo(3);
-  }
+export class AboutComponent implements AfterViewInit, OnInit, OnDestroy{
+
   @ViewChild('slickModal', {static: false}) slickModal: SlickCarouselComponent;
   title = 'ngSlick';
-  
+  teamMembers: TeamMembers[] = [];
+  private teamMembersSub: Subscription;
+  isLoading = false;
+  totalTeamMembers = 0;
+  postsPerPage = 10;
+  currentPage = 1;
+  pageSizeOptions = [1, 2 , 5, 10];
  
   slides = [
     {img: "http://placehold.it/350x350/777777"},
@@ -29,8 +36,9 @@ export class AboutComponent implements AfterViewInit{
     "slidesToShow": 3, 
     "slidesToScroll": 1,
     "dots":true,
-    "autoplay":true, "autoplaySpeed": 1500,
+    "autoplay":true, "autoplaySpeed": 1000,
     "infinite": true,
+    "arrows": false,
     "responsive": [
       {
         breakpoint: 800,
@@ -49,10 +57,27 @@ export class AboutComponent implements AfterViewInit{
       // instead of a settings object
     ]
   };
-  constructor(){
+  constructor(public teamMembersService:TeamMembersService){
    
   }
-  
+  ngOnInit() {
+    this.isLoading = true;
+    this.teamMembersService.getTeamMembers(this.postsPerPage, this.currentPage);
+    this.teamMembersSub = this.teamMembersService.getTeamMemberUpdateListener()
+      .subscribe(
+        (teamMemberData:{ teamMembers : TeamMembers[] ,postCount : number } ) => {
+          this.isLoading = false;
+          this.totalTeamMembers = teamMemberData.postCount;
+          this.teamMembers = teamMemberData.teamMembers;
+          console.log("teamMembers", this.teamMembers);
+        });
+  }
+  ngOnDestroy() {
+    this.teamMembersSub.unsubscribe();
+  }
+  ngAfterViewInit(): void {
+    this.slickModal.slickGoTo(3);
+  }
   addSlide() {
     this.slides.push({img: "http://placehold.it/350x150/777777"})
   }
